8f45194f9e03e4f4d0404a26954b8ab7174e5af4
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-08-21 19:26:35 +0300

    Tests (#1)
    * Lesson 06: added dependencies for testing
    
    * Lesson 06: introduced utils
    
    * Lesson 06: added unit and functional tests
    
    * Lesson 7: unittests, informative page
    
    * Lesson 7: tests, styles
    
    * Lesson 8: static, tests, dicts
    
    * Lesson 8: refactoring

--- server.py
+++ server.py
@@ -1,18 +1,35 @@
+import traceback
 from http.server import SimpleHTTPRequestHandler
 
 import settings
+from errors import MethodNotAllowed
+from errors import NotFound
+from utils import normalize_path
+from utils import read_static
+from utils import to_bytes
 
 
 class MyHttp(SimpleHTTPRequestHandler):
     def do_GET(self):
-        path = self.build_path()
+        path = normalize_path(self.path)
 
-        if path == "/":
-            self.handle_root()
-        elif path == "/hello/":
-            self.handle_hello()
-        else:
+        handlers = {
+            "/": self.handle_root,
+            "/hello/": self.handle_hello,
+            "/style/": self.handle_style,
+            "/logo/": self.handle_logo,
+            "/0/": self.handle_zde,
+        }
+
+        try:
+            handler = handlers[path]
+            handler()
+        except (NotFound, KeyError):
             self.handle_404()
+        except MethodNotAllowed:
+            self.handle_405()
+        except Exception:
+            self.handle_500()
 
     def handle_root(self):
         return super().do_GET()
@@ -30,23 +47,34 @@ class MyHttp(SimpleHTTPRequestHandler):
 
         self.respond(content)
 
+    def handle_zde(self):
+        x = 1 / 0
+
+    def handle_style(self):
+        css = read_static("styles/style.css")
+        self.respond(css, content_type="text/css")
+
+    def handle_logo(self):
+        image = read_static("images/logo.svg")
+        self.respond(image, content_type="image/svg+xml")
+
     def handle_404(self):
         msg = """NOT FOUND!!!!!!!!"""
 
         self.respond(msg, code=404, content_type="text/plain")
 
+    def handle_405(self):
+        self.respond("", code=405, content_type="text/plain")
+
+    def handle_500(self):
+        self.respond(traceback.format_exc(), code=500, content_type="text/plain")
+
     def respond(self, message, code=200, content_type="text/html"):
+        payload = to_bytes(message)
+
         self.send_response(code)
         self.send_header("Content-type", content_type)
-        self.send_header("Content-length", str(len(message)))
+        self.send_header("Content-length", str(len(payload)))
         self.send_header("Cache-control", f"max-age={settings.CACHE_AGE}")
         self.end_headers()
-        self.wfile.write(message.encode())
-
-    def build_path(self) -> str:
-        result = self.path
-
-        if result[-1] != "/":
-            result = f"{result}/"
-
-        return result
+        self.wfile.write(payload)
--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
 import os
+from pathlib import Path
 
 PORT = int(os.getenv("PORT", 8000))
 print(PORT)
 
 CACHE_AGE = 60 * 60 * 24
+
+PROJECT_DIR = Path(__file__).parent.resolve()
+STATIC_DIR = PROJECT_DIR / "static"
