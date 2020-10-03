e554bead53a514ac4c2a4a4cde951177d3a68d8d
Alexander Sidorov <alexander@sidorov.dev>
2020-10-03 16:29:41 +0300

    refactored views

--- src/applications/hello/forms/fields.py
+++ src/applications/hello/forms/fields.py
@@ -7,6 +7,9 @@ class NameField(CharField):
     default_validators = [validate_name]
 
     def __init__(self, *args, **kwargs):
-        kwargs_default = dict(min_length=3, max_length=1000, )
+        kwargs_default = dict(
+            min_length=3,
+            max_length=1000,
+        )
         kwargs.update(kwargs_default)
         super().__init__(*args, **kwargs)
