c5b3d91616264b8948429ff243e916a0ab6ae18a
Alexander Sidorov <alexander@sidorov.dev>
2020-10-07 21:19:08 +0300

    ssss

--- src/applications/hello/urls.py
+++ src/applications/hello/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from applications.hello.apps import HelloConfig
+from applications.hello.views import HelloView
 from applications.hello.views import view_index
 from applications.hello.views import view_reset
 from applications.hello.views import view_update
@@ -8,7 +9,7 @@ from applications.hello.views import view_update
 app_name = HelloConfig.label
 
 urlpatterns = [
-    path("", view_index, name="index"),
-    path("update/", view_update, name="update"),
+    path("", HelloView.as_view(), name="index"),
+    path("update/", HelloView.as_view(), name="update"),
     path("reset/", view_reset, name="reset"),
 ]
