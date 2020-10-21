e2f6dd4255923ed0d50081a2d6ea747a2043cf0d
Alexander Sidorov <alexander@sidorov.dev>
2020-10-21 19:48:01 +0300

    fixed db url

--- src/project/settings.py
+++ src/project/settings.py
@@ -59,9 +59,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "project.wsgi.application"
 
-database_url = _ds.DATABASE_URL
-if _ds.ENV_FOR_DYNACONF == "heroku":
-    database_url = os.getenv("DATABASE_URL")
+database_url = os.getenv("DATABASE_URL", _ds.DATABASE_URL)
 
 DATABASES = {
     "default": dj_database_url.parse(database_url),
