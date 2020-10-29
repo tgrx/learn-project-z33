03762f751bfb7c4abffb2c94e29765723d222044
Alexander Sidorov <alexander@sidorov.dev>
2020-10-29 18:46:48 +0300

    refactored templates

---
+++ src/applications/blog/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.TextField(unique=True)
+    content = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(default=datetime.now)
+    visible = models.BooleanField(default=False)
+
+    def __str__(self):
+        msg = f"'{self.title}', visible? {self.visible}"
+        return msg
--- src/applications/blog/views/index.py
+++ src/applications/blog/views/index.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from applications.blog.models import Post
+
 
 class IndexView(TemplateView):
     template_name = "blog/index.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        ctx["object_list"] = Post.objects.filter(visible=True)
+
+        return ctx
