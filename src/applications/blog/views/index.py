a9ab85858f95a58504a9c8e5d67c42c52d9924ea
Alexander Sidorov <alexander@sidorov.dev>
2020-11-04 19:22:44 +0300

    using ListView

--- src/applications/blog/views/index.py
+++ src/applications/blog/views/index.py
@@ -1,14 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from applications.blog.models import Post
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "blog/index.html"
-
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-
-        ctx["object_list"] = Post.objects.filter(visible=True)
-
-        return ctx
+    queryset = Post.objects.filter(visible=True)
